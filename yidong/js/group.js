eb6ad6bb9a7ca511b45b11c74fbcdc218d717527
liushinely <1326043621@qqcom>
2018-11-13 20:54:01 +0800

    提交的文件

---
+++ yidong/js/group.js
@@ -0,0 +1,10 @@
+var mySwiper=new Swiper(".swiper-one",{
+    autoplay:true,
+    loop : true,
+    pagination: {
+    el: '.swiper-pagination',
+},
+autoplay: {
+    disableOnInteraction: false,
+},
+})
